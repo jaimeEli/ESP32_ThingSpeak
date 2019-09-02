b6f0919cc90cfd2ca64fb57957d3f3e796c2faaa
antoniofuentesf <53945012+antoniofuentesf@users.noreply.github.com>
2019-09-02 09:22:39 -0500

    Update ThingSpeak_ESP32_DHT22.ino

--- ThingSpeak_ESP32_DHT22/ThingSpeak_ESP32_DHT22.ino
+++ ThingSpeak_ESP32_DHT22/ThingSpeak_ESP32_DHT22.ino
@@ -9,11 +9,11 @@
  * =========================================================
  */
 
-#define SECRET_SSID "Totalplay-2467"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "8466198497Santa" // replace MyPassword with your WiFi password
+#define SECRET_SSID ""    // replace MySSID with your WiFi network name
+#define SECRET_PASS "" // replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 856645 // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "8CSZX2RLSR5UHVL4"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 0000000 // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
 
 
 char ssid[] = SECRET_SSID;   // your network SSID (name) 
